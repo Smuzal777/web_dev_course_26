7cfdf5e35be98fe2aca45a8b41c621279f33ae6f
Соня <104689107+Smuzal777@users.noreply.github.com>
2026-02-12 15:05:23 +0000

    c4_hashes

--- labs/lab1/c4_hashes.rb
+++ labs/lab1/c4_hashes.rb
@@ -11,7 +11,7 @@
 # Expected: get_value({ name: "Alice", age: 25 }, :name) => "Alice"
 def get_value(hash, key)
   # TODO: Access hash value using hash[key]
-  nil
+  hash[key]
 end
 
 # Exercise 2: Hash modification
@@ -19,15 +19,16 @@ end
 # Expected: add_pair({ a: 1 }, :b, 2) => { a: 1, b: 2 }
 def add_pair(hash, key, value)
   # TODO: Add new pair using hash[key] = value
-  nil
-end
+  hash[key] = value
+  return hash
+end 
 
 # Exercise 3: Hash keys and values
 # Return an array of all keys from a hash
 # Expected: get_keys({ name: "Bob", age: 30, city: "NYC" }) => [:name, :age, :city]
 def get_keys(hash)
   # TODO: Use .keys method
-  nil
+  return hash.keys
 end
 
 # Exercise 4: Hash iteration
@@ -35,7 +36,8 @@ end
 # Expected: double_values({ a: 1, b: 2, c: 3 }) => { a: 2, b: 4, c: 6 }
 def double_values(hash)
   # TODO: Use .transform_values or .each_with_object
-  nil
+  new_hash = hash.transform_values{|elem| elem * 2}
+  return new_hash
 end
 
 # Exercise 5: Hash filtering
@@ -43,7 +45,7 @@ end
 # Expected: filter_by_value({ a: 10, b: 5, c: 15 }, 8) => { a: 10, c: 15 }
 def filter_by_value(hash, threshold)
   # TODO: Use .select method
-  nil
+  hash.select{|key, value| value > threshold}
 end
 
 # Exercise 6: Hash merging
@@ -51,7 +53,7 @@ end
 # Expected: merge_hashes({ a: 1, b: 2 }, { b: 3, c: 4 }) => { a: 1, b: 3, c: 4 }
 def merge_hashes(hash1, hash2)
   # TODO: Use .merge method
-  nil
+  hash1.merge(hash2)
 end
 
 # Exercise 7: Hash existence check
@@ -59,7 +61,7 @@ end
 # Expected: has_key?({ name: "Alice", age: 25 }, :name) => true
 def has_key?(hash, key)
   # TODO: Use .has_key? or .key? method
-  nil
+  hash.has_key?(key)
 end
 
 # Exercise 8: Working with nested hashes
@@ -67,7 +69,7 @@ end
 # Expected: nested_value({ user: { name: "John", age: 30 } }, :user, :name) => "John"
 def nested_value(hash, key1, key2)
   # TODO: Access nested hash using hash[key1][key2] or .dig method
-  nil
+  hash[key1][key2]
 end
 
 # ============================================
